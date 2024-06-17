6843f000cd66ee1b18efacf0f9f9374b439aa378
Idle <651462+reonZ@users.noreply.github.com>
2024-06-17 17:03:11 +0200

    add text popup

---
+++ src/hud/popup/text.ts
@@ -0,0 +1,27 @@
+import { createHTMLElement } from "foundry-pf2e";
+import { PF2eHudPopup, PopupConfig } from "./base";
+
+class PF2eHudTextPopup extends PF2eHudPopup<TextPopupConfig> {
+    get title() {
+        return `${this.actor.name} - ${this.config.title}`;
+    }
+
+    get content() {
+        return this.config.content;
+    }
+
+    async _renderHTML(context: never, options: ApplicationRenderOptions): Promise<HTMLElement> {
+        const content = await TextEditor.enrichHTML(this.content);
+        return createHTMLElement("div", {
+            innerHTML: content,
+            classes: ["text-content"],
+        });
+    }
+}
+
+type TextPopupConfig = PopupConfig & {
+    title: string;
+    content: string;
+};
+
+export { PF2eHudTextPopup };
