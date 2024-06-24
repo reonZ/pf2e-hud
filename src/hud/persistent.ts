d522df8257f5e80530fa4038a65fbdc4b6c37035
Idle <651462+reonZ@users.noreply.github.com>
2024-06-24 13:46:09 +0200

    better position Attack Popup

--- src/hud/persistent.ts
+++ src/hud/persistent.ts
@@ -1119,7 +1119,7 @@ class PF2eHudPersistent extends makeAdvancedHUD(
             case "open-attack-popup": {
                 if (actor.isOfType("character")) {
                     const { left, top, height } = this.mainElement!.getBoundingClientRect();
-                    openAttackpopup(actor, el.dataset, { left, top: top - height * 2 });
+                    openAttackpopup(actor, el.dataset, { left, top: top - height - 100 });
                 }
                 break;
             }
