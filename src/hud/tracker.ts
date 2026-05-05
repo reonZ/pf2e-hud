f0ce11c84a6f8d4ed2df5cb04cc962610add8ae6
Idle <651462+reonZ@users.noreply.github.com>
2026-05-05 20:42:19 +0200

    use package initiative functions

--- src/hud/tracker.ts
+++ src/hud/tracker.ts
@@ -22,7 +22,9 @@ import {
     R,
     render,
     RolledCombatant,
+    saveNewInitiativeOrder,
     setFlag,
+    setInitiativeFromDrop,
     settingPath,
     SYSTEM,
     toggleHooksAndWrappers,
@@ -877,59 +879,11 @@ class TrackerPF2eHUD extends BasePF2eHUD<TrackerSettings> {
 
         this.#cancelScroll = true;
 
-        this.#setInitiativeFromDrop(newOrder, dropped);
-        await this.#saveNewOrder(newOrder);
+        setInitiativeFromDrop(this.viewed, newOrder, dropped);
+        await saveNewInitiativeOrder(this.viewed, newOrder);
 
         if (nextTurn) {
             await encounter.nextTurn();
-        }
-    }
-
-    /**
-     * https://github.com/foundryvtt/pf2e/blob/a3856b6ae9c0427267b410bb81ff8d4cfefbeab4/src/module/apps/sidebar/encounter-tracker.ts#L376
-     */
-    async #saveNewOrder(newOrder: RolledCombatant<EncounterPF2e>[]): Promise<void> {
-        await this.viewed?.setMultipleInitiatives(
-            newOrder.map((c) => ({
-                id: c.id,
-                value: c.initiative,
-                overridePriority: c.overridePriority(c.initiative),
-            })),
-        );
-    }
-
-    /**
-     * https://github.com/foundryvtt/pf2e/blob/a3856b6ae9c0427267b410bb81ff8d4cfefbeab4/src/module/apps/sidebar/encounter-tracker.ts#L341
-     */
-    #setInitiativeFromDrop(newOrder: RolledCombatant<EncounterPF2e>[], dropped: RolledCombatant<EncounterPF2e>): void {
-        const aboveDropped = newOrder.find((c) => newOrder.indexOf(c) === newOrder.indexOf(dropped) - 1);
-        const belowDropped = newOrder.find((c) => newOrder.indexOf(c) === newOrder.indexOf(dropped) + 1);
-
-        const hasAboveAndBelow = !!aboveDropped && !!belowDropped;
-        const hasAboveAndNoBelow = !!aboveDropped && !belowDropped;
-        const hasBelowAndNoAbove = !aboveDropped && !!belowDropped;
-        const aboveIsHigherThanBelow = hasAboveAndBelow && belowDropped.initiative < aboveDropped.initiative;
-        const belowIsHigherThanAbove = hasAboveAndBelow && belowDropped.initiative < aboveDropped.initiative;
-        const wasDraggedUp =
-            !!belowDropped && this.viewed?.getCombatantWithHigherInit(dropped, belowDropped) === belowDropped;
-        const wasDraggedDown = !!aboveDropped && !wasDraggedUp;
-
-        // Set a new initiative intuitively, according to allegedly commonplace intuitions
-        dropped.initiative =
-            hasBelowAndNoAbove || (aboveIsHigherThanBelow && wasDraggedUp)
-                ? belowDropped.initiative + 1
-                : hasAboveAndNoBelow || (belowIsHigherThanAbove && wasDraggedDown)
-                  ? aboveDropped.initiative - 1
-                  : hasAboveAndBelow
-                    ? belowDropped.initiative
-                    : dropped.initiative;
-
-        const withSameInitiative = newOrder.filter((c) => c.initiative === dropped.initiative);
-        if (withSameInitiative.length > 1) {
-            for (let priority = 0; priority < withSameInitiative.length; priority++) {
-                const flag = withSameInitiative[priority].flags[SYSTEM.id];
-                flag.overridePriority[dropped.initiative] = priority;
-            }
         }
     }
 
